b09adeeae63a0663e3f0e402259db543dd0b838e
naive <582865471@qq.com>
2016-10-28 13:09:28 +0800

    add sword_refers_to_offer

---
+++ sword_refers_to_offer/high_quality_code/print_one_to_max_digit.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+    > File Name: print_one_to_max_digit.cpp
+    > Author: ma6174
+    > Mail: ma6174@163.com 
+    > Created Time: 2016年10月28日 星期五 13时00分14秒
+ ************************************************************************/
+
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	cout << "Hello, World!" << endl;
+
+	return 0;
+}
