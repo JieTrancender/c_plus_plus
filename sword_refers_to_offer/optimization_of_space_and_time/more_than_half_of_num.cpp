2ec84760b89fe60bae2add80d18b6f64331eed7e
jie-trancender <582865471@qq.com>
2016-11-09 22:08:23 +0800

    add timer_test.hpp

--- sword_refers_to_offer/optimization_of_space_and_time/more_than_half_of_num.cpp
+++ sword_refers_to_offer/optimization_of_space_and_time/more_than_half_of_num.cpp
@@ -9,3 +9,38 @@
 
 using namespace std;
 
+int partition(int* num, int length, int start, int end)
+{
+    if (num == nullptr || length <= 0 || start < 0 || end >= length)
+    {
+        throw new std::exception("Invalid Parameters");  //不合法参数
+    }
+
+    int index = num[0];
+
+    int small = start;
+    for (index = start + 1; index < end; ++index)
+    {
+        if (num[index] < num[start])
+        {
+            ++small;
+            if (index != start)
+            {
+                std::swap(num[index], num[small]);
+            }
+        }
+    }
+
+    ++small;
+    std::swap(num[small], num[start]);
+
+    return small;
+}
+
+bool g
+
+int moreThanHalfOfNum(int* num, int length)
+{
+    if (checkInvalidArray(num, length))
+        return 0;
+}
