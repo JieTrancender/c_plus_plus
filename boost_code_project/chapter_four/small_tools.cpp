60818d3756fb7b30ce6eda2e8bd1cb388f590480
jie-trancender <582865471@qq.com>
2016-11-21 23:28:51 +0800

    update

--- boost_code_project/chapter_four/small_tools.cpp
+++ boost_code_project/chapter_four/small_tools.cpp
@@ -6,6 +6,8 @@
  ************************************************************************/
 
 #include <iostream>
+#include <string>
+#include <vector>
 #include <boost/core/noncopyable.hpp>
 #include <boost/core/ignore_unused.hpp>
 #include <boost/optional.hpp>
@@ -24,9 +26,38 @@ namespace mtl
         const noncopyable& operator=(const noncopyable&) = delete;
     };
 
-    struct none_helper(){};
+    struct none_helper{};
     typedef int none_helper::*none_t;
     none_t const none = (static_cast<none_t>(0));  //定义常亮none
+
+    template <typename T>
+    class optional
+    {
+    public:
+        optional();
+        optional(none_t);
+        optional(T const& v);
+        optional(bool condition, T v);
+
+        optional& operator=(T const& rhs);
+
+        template <typename... Args>
+        void emplace(Args...&& args);
+
+        T* operator->();
+        T& operator*();
+
+        T& get();
+        T* get_ptr();
+
+        T& value();
+        T const& value_or(T const& default) const;
+        template <typename F>
+        T value_or_eval(F f) const;
+
+        explicit operator bool() const;
+        bool operator!() const;
+    };
 
     //ignore_unused基本用法
     template <typename... T>
@@ -59,8 +90,33 @@ int main(int argc, char* argv[])
 {
     //empty_class empty_obj;
     //empty_class empty_obj1(empty_obj);
-    func(3, 4);
-    func2();
+    //func(3, 4);
+    //func2();
+
+    optional<int> op0;
+    optional<int> op1(none);
+
+    assert(!op0);
+    assert(op0 == op1);
+
+    assert(op1.value_or(253) == 253);
+    std:cout << op1.value_or_eval([](){ return 874;  }) << std::endl;
+
+    optional<std::string> ops("test");
+    std:cout << *ops << std::endl;
+
+    ops.emplace("monado", 3);
+    assert(*ops == "mon");
+
+    std::vector<int> v(10);
+    optional<std::vector<int>& > opv(v);
+    assert(opv);
+
+    opv->push_back(5);
+    assert(opv->size() == 11);
+
+    opv = none;
+    assert(!opv);
 
 }
 
---
+++ boost_code_project/chapter_four/test.cpp
@@ -0,0 +1,20 @@
+/*************************************************************************
+	> File Name: test.cpp
+	> Author: JieTrancender
+	> Mail: jie-email@jie-trancender.org
+	> Created Time: Sun Nov 20 23:05:11 2016
+ ************************************************************************/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 3, b = 4;
+
+    if (a < 2, b > 2)
+    {
+        cout << __FILE__ << ':' << __LINE__ << '#' << __FUNCTION__ << endl;
+    }
+}
